11959d00a39ab9f4baaae2eb74ea324df656e89f
lethevu <kevinlethevu@gmail.com>
2019-02-03 23:54:33 -0800

    Initial Commit

---
+++ instagramwithfirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagramwithfirebase
+//
+//  Created by kevin le on 2/3/19.
+//  Copyright © 2019 kevin le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
